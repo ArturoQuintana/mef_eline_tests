1097152b10555674c182e62f2f8d35cc170379b9
Roicxy Alonso Gonzalez <ralon051@fiu.edu>
2019-06-17 12:15:14 -0400

    Create Roles to test them

--- roles/create_evc/files/evc_request.py
+++ roles/create_evc/files/evc_request.py
@@ -3,10 +3,53 @@
 import requests
 import os
 import sys
-from roles.create_evc.files.links import ControllerConnection
 
 
+class ControllerConnection():
+    """Class to connect to the controller and extract the information."""
 
+    ip_addr = ""
+    ip_port = int
+
+    def __init__(self, ip_addr="", ip_port=int):
+        """Initialize the the class with the controller information.
+
+        Args:
+            ip_addr(str): The ip address for the controller.
+            ip_port(int): The port to connect to the controller.
+        """
+        self.ip_addr = ip_addr
+        self.ip_port = ip_port
+
+    def getting_ids(self):
+        """Create a dictionary from the switches connected
+        and extract the switch's IDs."""
+
+        requested_app_info = "/api/kytos/topology/v3/switches"
+
+        addr_inf = "http://{}:{}{}".format(self.ip_addr,
+                                           self.ip_port,
+                                           requested_app_info)
+
+        try:
+
+            json_data = requests.get(addr_inf).json()
+
+            switch_ids = ""
+
+            for switch in dict(json_data['switches']).keys():
+
+                swt_id_port = 10
+
+                switch_ids += "{}:{}\n".format(switch, swt_id_port)
+
+            list_ids = list(switch_ids)
+
+            return list_ids
+
+        except Exception as e:
+
+            print(e)
 
 class Request():
     """
